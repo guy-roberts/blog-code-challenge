d86e29173a74c157bc0a54d596a1ab00c2b950b1
Guy <Guy@www.guyroberts.co.uk>
2015-05-02 17:58:35 +0100

    Mockups New Rails app generated with 'rails-api new my_api' Database configured rails generate devise:install rails generate devise User rake db:migrate

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MyApi::Application.config.secret_token = 'cfe352e840243ebc415d180866b927cb4dec38de3a2f85f8e62a64ee6d7b0b7b21b6b9a60510826c7afd231454d41c1dd391de0f102ced729fcde4dc0d080fae'
