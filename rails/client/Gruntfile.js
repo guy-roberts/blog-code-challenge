8326a1d3947d50391ce0e8f4fdf8cc3dbb775b50
Guy <Guy@www.guyroberts.co.uk>
2015-05-05 08:51:31 +0100

    Don't need ngmin

--- rails/client/Gruntfile.js
+++ rails/client/Gruntfile.js
@@ -365,6 +365,20 @@ module.exports = function (grunt) {
       }
     },
 
+    // ngmin tries to make the code safe for minification automatically by
+    // using the Angular long form for dependency injection. It doesn't work on
+    // things like resolve or inject so those have to be done manually.
+    ngmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/concat/scripts',
+          src: '*.js',
+          dest: '.tmp/concat/scripts'
+        }]
+      }
+    },
+
     // ng-annotate tries to make the code safe for minification automatically
     // by using the Angular long form for dependency injection.
     ngAnnotate: {
@@ -490,7 +504,7 @@ module.exports = function (grunt) {
     'svgmin',
     'autoprefixer',
     'concat',
-    'ngmin',
+    //'ngmin',
     'ngAnnotate',
     'copy:dist',
     'cdnify',
